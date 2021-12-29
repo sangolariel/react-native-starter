e5fc94d9897b063fc7cff878fda23a425bf6c0da
sangnh <sangnh@futurify.vn>
2021-12-29 11:19:18 +0700

    config project with typescript

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests'],
+          '~': './src',
+          '@components': './src/theme/components',
+          '@interfaces': './src/interfaces',
+          '@containers': './src/theme/containers',
+          '@themeConfig': './src/theme/configs',
+          '@svg': './src/theme/svg',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
 };
