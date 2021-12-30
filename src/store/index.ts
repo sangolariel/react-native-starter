c377b29301da85d9ebc854f1aa6406f1ea346299
sangnh <sangnh@futurify.vn>
2021-12-30 16:56:05 +0700

    config project

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ import rootSaga from './sagas';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['common', 'language', 'storage'],
+  whitelist: ['common'],
 };
 
 const composeEnhancers: any =
