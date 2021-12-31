644a3e875342049a61f90dace24a952783fb7a95
sangnh <sangnh@futurify.vn>
2021-12-31 11:39:58 +0700

    Run the app with TS ok

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export interface Props {}
+
+const App: React.FC<Props> = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+  },
+});
+
+export default App;
