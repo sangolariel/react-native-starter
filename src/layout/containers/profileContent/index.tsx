64d34df4a39f0669e1bbabb1fc8def020f1749d1
sangnh <sangnh@futurify.vn>
2022-01-04 17:07:02 +0700

    add ProfileContent

---
+++ src/layout/containers/profileContent/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {
+  Avatar,
+  Caption,
+  Switch,
+  Text,
+  Title,
+  TouchableRipple,
+  useTheme,
+  Drawer,
+} from 'react-native-paper';
+
+import {PreferencesContext} from '~/contexts/preferencesContext';
+
+export default function ProfileContent() {
+  const paperTheme = useTheme();
+  const {rtl, theme, toggleRTL, toggleTheme} =
+    React.useContext(PreferencesContext);
+
+  return (
+    <View
+      //@ts-ignore
+      style={[
+        styles.drawerContent,
+        {
+          backgroundColor: paperTheme.colors.surface,
+        },
+      ]}>
+      <View style={styles.userInfoSection}>
+        <Avatar.Image
+          source={{
+            uri: 'https://haymora.com/upload/files/cong_nghe_thong_tin/2019/futurify/logo-cong-ty-futurify.jpg',
+          }}
+          size={50}
+        />
+        <Title style={styles.title}>Sang Nguyen</Title>
+        <Caption style={styles.caption}>sangnh@futurify.vn</Caption>
+      </View>
+      <Drawer.Section>
+        <TouchableRipple onPress={toggleTheme}>
+          <View style={styles.preference}>
+            <Text>Dark Theme</Text>
+            <View pointerEvents="none">
+              <Switch value={theme === 'dark'} />
+            </View>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={toggleRTL}>
+          <View style={styles.preference}>
+            <Text>RTL</Text>
+            <View pointerEvents="none">
+              <Switch value={rtl === 'right'} />
+            </View>
+          </View>
+        </TouchableRipple>
+      </Drawer.Section>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    marginTop: 20,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: 'bold',
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  preference: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+  sections: {
+    paddingTop: 25,
+  },
+});
--- src/layout/screens/profile/index.tsx
+++ src/layout/screens/profile/index.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+
+import ProfileContent from '~/layout/containers/profileContent';
 
 type Props = {};
 
 const Profile: React.FC<Props> = () => {
   return (
-    <View style={styles.container}>
-      <Text>Profile Screen</Text>
-    </View>
+    <React.Fragment>
+      <ProfileContent />
+    </React.Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16,
-  },
-});
 
 export default Profile;
