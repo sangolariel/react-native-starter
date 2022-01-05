b30720d5d25b06cfc6d9d271e7370d7033409460
sangnh <sangnh@futurify.vn>
2022-01-05 15:21:26 +0700

    add Feeds Store

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 import commonReducer from './common/reducer';
 import languageReducer from './language/reducer';
+import feedReducer from './feed/reducer';
 
 /**
  * Root reducer
@@ -9,6 +10,7 @@ import languageReducer from './language/reducer';
 const rootReducers = combineReducers({
   common: commonReducer,
   language: languageReducer,
+  feed: feedReducer,
 });
 
 export default rootReducers;
--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -1,9 +1,11 @@
 import {all} from 'redux-saga/effects';
 import commonSaga from './common/saga';
+import feedSaga from './feed/saga';
+
 /**
  * Root saga
  * @returns {IterableIterator<AllEffect | GenericAllEffect<any> | *>}
  */
 export default function* rootSagas() {
-  yield all([commonSaga()]);
+  yield all([commonSaga(), feedSaga()]);
 }
