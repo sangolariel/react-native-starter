62798734e345e4f84e60836bfb48d37868e7acdb
sangnh <sangnh@futurify.vn>
2022-01-05 09:15:22 +0700

    add layout

---
+++ src/layout/containers/messegerContent/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {Headline, Caption, useTheme, Button} from 'react-native-paper';
+
+type Props = {
+  navigation: BottomTabNavigationProp<{}>;
+};
+const Message = () => {
+  const theme = useTheme();
+
+  const backgroundColor = theme.colors.surface;
+
+  return (
+    <ScrollView
+      style={{backgroundColor}}
+      contentContainerStyle={[styles.scrollViewContent, {backgroundColor}]}>
+      <Headline style={styles.centerText}>
+        Send a message, get a message
+      </Headline>
+      <Caption style={styles.centerText}>
+        Private Messages are private conversations between you and other people
+        on Twitter. Share Tweets, media, and more!
+      </Caption>
+      <Button
+        onPress={() => {}}
+        style={styles.button}
+        mode="contained"
+        labelStyle={styles.labelStyle}>
+        Write a message
+      </Button>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    flex: 1,
+    paddingHorizontal: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    textAlign: 'center',
+  },
+  button: {
+    marginTop: 20,
+  },
+  labelStyle: {
+    color: 'white',
+  },
+});
+
+export default Message;
--- src/layout/screens/messenger/index.tsx
+++ src/layout/screens/messenger/index.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+
+import MessageContent from '~/layout/containers/messegerContent';
 
 type Props = {};
 
 const Messenger: React.FC<Props> = () => {
   return (
-    <View style={styles.container}>
-      <Text>Messenger Screen</Text>
-    </View>
+    <React.Fragment>
+      <MessageContent />
+    </React.Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  greeting: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 16,
-  },
-});
 
 export default Messenger;
