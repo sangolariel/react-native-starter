142e5e83bd7391d6ee91cf726e3c376b88892c97
sangnh <sangnh@futurify.vn>
2022-01-06 17:06:16 +0700

    clear refNavigator

--- src/utils/refNavigator.ts
+++ src/utils/refNavigator.ts
@@ -1,40 +1,8 @@
 import React, {RefObject} from 'react';
-import {
-  NavigationContainerRef,
-  StackActions,
-  TabActions,
-} from '@react-navigation/native';
+import {NavigationContainerRef} from '@react-navigation/native';
 
 import {IObject} from '~/types/common';
 
 export const navigationRef:
   | RefObject<NavigationContainerRef<any>>
   | IObject<any> = React.createRef();
-
-export function navigate(name: string, params?: IObject<any>) {
-  navigationRef.current?.navigate(name, params);
-}
-
-export function replace(name: string, params?: IObject<any>) {
-  navigationRef?.current.dispatch(StackActions.replace(name, params));
-}
-
-export function nestedNavigate(
-  parentName: string,
-  name: string,
-  params?: IObject<any>,
-) {
-  navigationRef.current?.navigate(parentName, {
-    screen: name,
-    params: params,
-  });
-}
-
-export function goBack() {
-  navigationRef?.current.goBack();
-}
-
-export function jumpTo(name: string, params?: IObject<any>) {
-  const jumpToAction = TabActions.jumpTo(name, params);
-  navigationRef?.current.dispatch(jumpToAction);
-}
