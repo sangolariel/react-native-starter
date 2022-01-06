537f0b37dedbc9ba75a420c5bfba1a3f96c568f5
sangnh <sangnh@futurify.vn>
2022-01-06 09:53:37 +0700

    add api constants

---
+++ src/constants/api.ts
@@ -0,0 +1,16 @@
+/* API STATUS CODE */
+export const SUCCESS = 200;
+export const SUCCESS_201 = 201;
+export const SUCCESS_204 = 204;
+export const BAD_REQUEST = 400;
+export const SESSION_EXPIRED = 401;
+export const NOT_FOUND = 404;
+export const INTERNAL_SERVER_ERROR = 500;
+
+/* Query Params */
+export const offset = 0;
+export const limit = 12;
+
+/* API METHODS */
+export const POST = 'post';
+export const GET = 'get';
